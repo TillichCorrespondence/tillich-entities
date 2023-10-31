ce8109c758dab953f38b0cc9075e2642310918b0
csae8092 <peter.andorfer@oeaw.ac.at>
2023-10-31 11:25:21 +0100

    updated make_place_list.py

--- make_place_list.py
+++ make_place_list.py
@@ -9,18 +9,19 @@ doc = TeiReader(
 nsmap = doc.nsmap
 
 
-items = set()
+items = list()
 for x in doc.any_xpath(".//tei:placeName/text()"):
-    items.add(x)
+    items.append(x)
 
-for x in glob.glob("../tillich-briefe-data/data/editions/*.xml"):
+for x in glob.glob("../TillichEdition/data/*.xml"):
     doc = TeiReader(x)
-    for p in doc.any_xpath(".//tei:rs[@type='place' and @ref]/@ref"):
-        items.add(p[1:])
-
+    for p in doc.any_xpath(".//tei:placeName[@ref]/@ref"):
+        items.append(p)
+print(len(items))
+print(len(set(items)))
 
 data = []
-for i, x in enumerate(sorted(items), 1):
+for i, x in enumerate(sorted(set(items)), 1):
     tillich_id = f"tillich_place_id__{i}"
     data.append({"name": x, "tillich_id": tillich_id})
 
