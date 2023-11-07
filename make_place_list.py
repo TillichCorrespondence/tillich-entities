d67f32a7e2b7cb579fd7d8e977d8279614d2cfa4
csae8092 <peter.andorfer@oeaw.ac.at>
2023-11-07 16:46:29 +0100

    minor

--- make_place_list.py
+++ make_place_list.py
@@ -10,20 +10,34 @@ nsmap = doc.nsmap
 
 
 items = list()
+faulty = list()
 for x in doc.any_xpath(".//tei:placeName/text()"):
     items.append(x)
 
 for x in glob.glob("../TillichEdition/data/*.xml"):
-    doc = TeiReader(x)
+    try:
+        doc = TeiReader(x)
+    except Exception as e:
+        faulty.append([x, e])
+    for p in doc.any_xpath(".//tei:placeName[@ref]/@ref"):
+        items.append(p)
+
+for x in glob.glob("../TillichEdition/Data_unrefined/*.xml"):
+    try:
+        doc = TeiReader(x)
+    except Exception as e:
+        faulty.append([x, e])
     for p in doc.any_xpath(".//tei:placeName[@ref]/@ref"):
         items.append(p)
 print(len(items))
 print(len(set(items)))
+print(faulty)
 
 data = []
 for i, x in enumerate(sorted(set(items)), 1):
     tillich_id = f"tillich_place_id__{i}"
-    data.append({"name": x, "tillich_id": tillich_id})
+    item = {"name": x, "tillich_id": tillich_id, "geonames_url": ""}
+    data.append(item)
 
 df = pd.DataFrame(data)
 
